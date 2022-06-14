d9cfa038266bc3ea76b850bc190c59543cb095a1
octablast <103292705+octablast@users.noreply.github.com>
2022-06-14 07:44:24 -0500

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,25 @@
+from astropy.io import fits
+from astropy.table import Table
+from astropy.time import Time
+
+
+fileLocation = '/home/quadblast24/PythonPrograms/sw00049929013uuu_sk.img'
+with fits.open(fileLocation) as hdu:
+    l = len(hdu)
+    print('Checking ' + str(l-1) + ' extensions.')
+    for i in range(l):
+        if i>0:
+            print(str(i)+' ASPCORR = ' +str(hdu[i].header['ASPCORR']))
+        if i>1 and hdu[i].header['ASPCORR'] == 'NONE':
+            diff = (Time(hdu[i].header['DATE-OBS']).jd-
+                  Time(hdu[i-1].header['DATE-OBS']).jd)*24*60
+            print('Time difference (minutes) = ' +str(diff))
+            if diff < 60:
+                print('CDELT1/2 of '+str(i)+' replaced with CDELT1/2 of '+str(i-1))
+                fits.setval(fileLocation, 'CDELT1', value = hdu[i-1].header['CDELT1'], ext=i)
+                #print(str(hdu[i].header['CDELT1']) + ' changed to ' + str(hdu[i-1].header['CDELT1']))
+                fits.setval(fileLocation, 'CDELT2', value = hdu[i-1].header['CDELT2'], ext=i)
+                #print(str(hdu[i].header['CDELT2']) + ' changed to ' + str(hdu[i-1].header['CDELT2']))
+                
+                
+    
